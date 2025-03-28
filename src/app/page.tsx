f1760454bfdef7cbcf1d8f17ceab04bcb096b5cc
adith2005-20 <132918418+adith2005-20@users.noreply.github.com>
2025-03-28 17:34:25 +0530

    initialization and auth setup

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+import { LatestPost } from "@/app/_components/post";
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Home() {
+  
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        
+        
+      </main>
+    </HydrateClient>
+  );
+}
---
+++ src/server/db/schema.ts
@@ -0,0 +1,79 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { sql } from "drizzle-orm";
+import { pgTable, text, integer, timestamp, boolean, index, pgTableCreator } from "drizzle-orm/pg-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = pgTableCreator((name) => `verifytiktr_${name}`);
+
+// auth tables
+
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text('name').notNull(),
+email: text('email').notNull().unique(),
+emailVerified: boolean('email_verified').notNull(),
+image: text('image'),
+createdAt: timestamp('created_at').notNull(),
+updatedAt: timestamp('updated_at').notNull(),
+isVerified: boolean('is_verified').notNull().default(false)
+});
+
+export const session = pgTable("session", {
+  id: text("id").primaryKey(),
+  expiresAt: timestamp('expires_at').notNull(),
+token: text('token').notNull().unique(),
+createdAt: timestamp('created_at').notNull(),
+updatedAt: timestamp('updated_at').notNull(),
+ipAddress: text('ip_address'),
+userAgent: text('user_agent'),
+userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' })
+});
+
+export const account = pgTable("account", {
+  id: text("id").primaryKey(),
+  accountId: text('account_id').notNull(),
+providerId: text('provider_id').notNull(),
+userId: text('user_id').notNull().references(()=> user.id, { onDelete: 'cascade' }),
+accessToken: text('access_token'),
+refreshToken: text('refresh_token'),
+idToken: text('id_token'),
+accessTokenExpiresAt: timestamp('access_token_expires_at'),
+refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+scope: text('scope'),
+password: text('password'),
+createdAt: timestamp('created_at').notNull(),
+updatedAt: timestamp('updated_at').notNull()
+});
+
+export const verification = pgTable("verification", {
+  id: text("id").primaryKey(),
+  identifier: text('identifier').notNull(),
+value: text('value').notNull(),
+expiresAt: timestamp('expires_at').notNull(),
+createdAt: timestamp('created_at'),
+updatedAt: timestamp('updated_at')
+});
+
+
+// core logic tables
+
+export const posts = createTable(
+  "post",
+  (d) => ({
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    name: d.varchar({ length: 256 }),
+    createdAt: d
+      .timestamp({ withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  }),
+  (t) => [index("name_idx").on(t.name)],
+);
